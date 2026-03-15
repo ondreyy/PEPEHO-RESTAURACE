51822ce8c23574f273af21c6c9b07488c9e4bfdd
Ondřej Foltín <foltin@spstrutnov.cz>
2026-03-15 12:16:00 +0100

    Pepeho restaurace (2022)

---
+++ galerie.js
@@ -0,0 +1,112 @@
+        var modal = document.getElementById("myModal");
+        
+        var img = document.getElementById("myImg");
+        var img2 = document.getElementById("myImg2");
+        var img3 = document.getElementById("myImg3");
+        var img4 = document.getElementById("myImg4");
+        var img5 = document.getElementById("myImg5");
+        var img6 = document.getElementById("myImg6");
+        var img7 = document.getElementById("myImg7");
+        var img8 = document.getElementById("myImg8");
+        var img9 = document.getElementById("myImg9");
+        var img10 = document.getElementById("myImg10");
+        var img11 = document.getElementById("myImg11");
+        var img12 = document.getElementById("myImg12");
+
+        var modalImg = document.getElementById("img");
+
+        var captionText = document.getElementById("caption");
+        img.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+        img2.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img3.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img4.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img5.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img6.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img7.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img8.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img9.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img10.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img11.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        img12.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        
+        }
+
+        var span = document.getElementsByClassName("close")[0];
+        var modal2 = document.getElementsByClassName("modal")[0];
+
+
+        span.onclick = function() { 
+          modal.style.display = "none";
+        }
+
+        modal2.onclick = function() {
+          modal.style.display = "none";
+        }
---
+++ zabak.js
@@ -0,0 +1,19 @@
+function pepe() {
+    var x = document.getElementById("pepe");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } 
+    else {
+        x.style.display = "block";
+    }
+}
+
+function peperest() {
+    var x = document.getElementById("peperest");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } 
+    else {
+        x.style.display = "block";
+    }
+}
